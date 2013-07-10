c737a8067ea1a6f4420765c3510f2fb8e19a8099
Hack Reactor Students <github@hackreactor.com>
2013-07-10 11:37:53 -0700

    able to write to sites.txt

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,9 @@
+var fs = require('fs');
+
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
 
 exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  fs.writeFile('/Users/hackreactor/code/Shugardude/2013-06-web-historian/data/sites.txt', 'Hello Node', function () {
+      console.log('It\'s saved!');
+  });
 };
