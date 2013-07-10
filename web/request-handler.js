4b3a77bba5c5e0669b8638b9cd7fe2a09ba81bdf
Hack Reactor Students <github@hackreactor.com>
2013-07-10 11:48:47 -0700

    changed writeFile to appendFile

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,8 +2,22 @@ var fs = require('fs');
 
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
 
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
 exports.handleRequest = function (req, res) {
-  fs.writeFile('/Users/hackreactor/code/Shugardude/2013-06-web-historian/data/sites.txt', 'Hello Node', function () {
+  fs.appendFile('/Users/hackreactor/code/Shugardude/2013-06-web-historian/data/sites.txt', ', something', function () {
       console.log('It\'s saved!');
   });
+
+
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "application/json";
+
+  res.writeHead(200, headers);
+  res.end('');
 };
